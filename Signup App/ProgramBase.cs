4966e0f06d9bbeeca4fa818e79a22f8407e1c24e
Mukthar Abdulwaheed <mukhyolakunle@gmail.com>
2023-02-04 12:17:53 +0100

    Initial commit

---
+++ Signup App/ProgramBase.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Signup_App
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Write("Please choose one; 1. Signup 2. login: ");
+            string signUp = Console.ReadLine();
+
+
+            if (signUp == Convert.ToString(1))
+            {
+                Console.WriteLine("Input your First name:");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Input your Last name:");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Input your email address:");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Input your phone number:");
+                int phoneNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Input your password:");
+                string password1 = Console.ReadLine();
+                Console.WriteLine("Input your password again:");
+                string password2 = Console.ReadLine();
+                if (password1 == password2)
+                {
+                    Console.WriteLine("Correct, and good to continue");
+                }
+                else
+                {
+                    Console.WriteLine("Type your Password again");
+                }
+            }
+            else if (signUp == Convert.ToString(2))
+            {
+                Console.WriteLine("Input your email address:");
+                string emailAddress = Console.ReadLine();
+
+                Console.WriteLine("Input your Password:");
+                string password3 = Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("datascience" +"");
+            }
+            
+                    
+
+
+
+        }
+    }
+}
---
+++ Signup App/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signup_App
+{
+    internal class User
+    {
+        public string firstName;
+
+        public string lastName;
+
+        public string email;
+
+        public string password1;
+
+        public string password2;
+
+        public string phoneNumber;
+    }
+}
